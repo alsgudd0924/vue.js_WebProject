cb81e5507af027aff833af5de762c4ed2e0eb454
alsgud0924 <yoominhyung08@gmail.com>
2026-03-19 17:24:04 +0900

    remove vercel analytics

--- client/src/main.js
+++ client/src/main.js
@@ -2,9 +2,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-import { inject as vercelAnalytics } from '@vercel/analytics'
-
-vercelAnalytics()
 
 const app = createApp(App)
 app.use(createPinia())
