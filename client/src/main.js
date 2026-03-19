a659f12e0efbd3f72b5da40d41f15d3e0671c90e
alsgud0924 <yoominhyung08@gmail.com>
2026-03-19 16:59:45 +0900

    fix analytics

--- client/src/main.js
+++ client/src/main.js
@@ -2,9 +2,9 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-import { inject } from '@vercel/analytics'
+import { inject as vercelAnalytics } from '@vercel/analytics'
 
-inject()
+vercelAnalytics()
 
 const app = createApp(App)
 app.use(createPinia())
